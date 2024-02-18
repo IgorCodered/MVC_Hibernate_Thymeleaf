d07c18c42d5a3dfa603a7b120023c8a5b2364d5e
Igor Elesin <149936179+IgorCodered@users.noreply.github.com>
2024-02-19 00:08:54 +0700

    createUser.html

--- src/main/java/web/controllers/UserController.java
+++ src/main/java/web/controllers/UserController.java
@@ -4,13 +4,16 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import web.model.User;
 import web.service.UserService;
 
+import javax.validation.Valid;
 import java.util.List;
 
 
@@ -28,14 +31,16 @@ public class UserController {
     }
 
     @GetMapping("/users/createUser")
-    public String createPages() {
+    public String createPages(ModelMap map) {
+        User user = new User();
+        map.addAttribute("user", user);
         return "createUser";
     }
 
     @PostMapping("/users/create")
-    public String createUser(ModelMap model) {
-        User user = new User();
-        model.addAttribute("user", user);
+    public String createUser(@ModelAttribute("user") @Valid User user, BindingResult result) {
+        if (result.hasErrors()) return "createUser";
+        service.addUser(user);
         return "redirect:/users";
     }
 }
